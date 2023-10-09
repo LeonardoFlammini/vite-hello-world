ec0aaca1d1f57afc7ff0282087d5634b4365dd36
LeonardoFlammini <leonardo98flammini@gmail.com>
2023-10-09 14:45:56 +0200

    [add] component

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+
+<script>
+  export default {
+    name: 'AppTitle',
+    data(){
+      return {
+        title: "La mia prima app con Vite!"
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped >
+h1{
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
